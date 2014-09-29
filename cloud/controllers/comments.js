16cafa8ae61e796afda8d89c031740dcdc5fd78f
syllogismos <anilkaraka@live.in>
2014-09-29 23:17:26 +0530

    comments controller

---
+++ cloud/controllers/comments.js
@@ -0,0 +1,28 @@
+var _ = require('underscore');
+var Comment = Parse.Object.extend('Comment');
+
+exports.index = function(req, res) {
+	var query = new Parse.Query(Comment);
+	query.descending('createdAt');
+	query.find().then(function(results){
+		res.render('comments/index', {comments: results});
+	},
+	function() {
+		res.send(500, 'failed loading comments');
+	});
+};
+
+exports.create = function(req, res) {
+	var comment = new Comment();
+	var Post = Parse.Object.extend('post');
+	var post = new Post();
+	post.id = req.params.post_id;
+	comment.set('post', post);
+
+	comment.save(_.pick(req.body, 'author', 'author_email', 'body')).then(function() {
+		res.redirect('/posts/' + req.params.post_id);
+	},
+	function() {
+		res.send(500, 'failed saving comment');
+	})
+}
